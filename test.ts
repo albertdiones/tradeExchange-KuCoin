020dbb88a865a56b9ac7be7ac401cbbbca2bc76d
Albert Diones <adiones@addteam.org>
2024-08-23 22:33:00 +0800

    working getAssets()

---
+++ test.ts
@@ -0,0 +1,33 @@
+import Logger from "add_logger";
+import KuCoin from "./kucoin";
+import cacheViaRedis from "cache-via-redis";
+import XhrJson from "./xhrjson";
+
+
+class CacheViaNothing {
+    async getItem(key: string): Promise<string | null> {
+        return null;
+    }
+
+    setItem(
+        key: string, 
+        value: string,
+        expirationSeconds: number
+    ): void { 
+    }
+}
+
+
+const exchange = new KuCoin({
+    logger: new Logger('kucoin'), 
+    client: new XhrJson({
+        logger: new Logger('http_client'), 
+        cache: new CacheViaNothing(), 
+        minTimeoutPerRequest: 100,
+        maxRandomPreRequestTimeout: 2000
+    })
+});
+
+// ['BTC','ETH','XRP']
+const assets = await exchange.getAssets();
+console.log(assets.length, assets);
